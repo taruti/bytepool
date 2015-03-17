9d15196da505a5fd921d071cf853803f7dd0be36
Taru Karttunen <taruti@taruti.net>
2015-03-17 11:50:04 +0200

    Add test

--- bytepool_test.go
+++ bytepool_test.go
@@ -4,6 +4,31 @@ import (
 	"testing"
 )
 
+func TestBytepool(t *testing.T) {
+	for i := 0; i < 99; i++ {
+		bs := Alloc(i)
+		if len(bs) != i {
+			t.Fatal("Allocated slice has wrong length")
+		}
+		Free(bs[:0])
+	}
+	for i := 0; i < MaxSize; i += 3 + i/2 {
+		t.Logf("Alloc %d %x", i, i)
+		bs := Alloc(i)
+		if len(bs) != i {
+			t.Fatal("Allocated slice has wrong length")
+		}
+		Free(bs)
+	}
+	for i := 0; i < MaxSize; i += 3 + i/3 {
+		bs := Alloc(i)
+		if len(bs) != i {
+			t.Fatal("Allocated slice has wrong length")
+		}
+		Free(bs)
+	}
+}
+
 func BenchmarkAlloc256R(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		consumer(make([]byte, 256))
